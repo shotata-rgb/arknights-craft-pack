2f35b6c5da0c6acd90d9f0240a045e8e90b26861
shotata-rgb <shotata-rgb@users.noreply.github.com>
2026-09-01 16:32:00 +0900

    Fix: Replace gunpowder with originium dust in all recipes using KubeJS

---
+++ kubejs/server_scripts/replace_gunpowder.js
@@ -0,0 +1,1 @@
+﻿ServerEvents.recipes(event => { event.replaceInput({}, 'minecraft:gunpowder', 'oripathymod:originium_dust'); event.replaceInput({}, '#forge:gunpowder', 'oripathymod:originium_dust'); });
